2738d1b9ca8ff3bda15c6ad9d6b74bbccd58ed47
oleg <osn298@gmail.com>
2026-06-16 12:03:27 +0300

    register aliases

--- src/main/java/mineverse/Aust1n46/chat/alias/Alias.java
+++ src/main/java/mineverse/Aust1n46/chat/alias/Alias.java
@@ -1,23 +1,28 @@
 package mineverse.Aust1n46.chat.alias;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 
 import mineverse.Aust1n46.chat.MineverseChat;
 
-public class Alias {
+public class Alias extends Command {
     private static MineverseChat plugin = MineverseChat.getInstance();
     private static List<Alias> aliases;
 
-    private String name;
     private int arguments;
     private List<String> components;
     private String permission;
 
     public Alias(String name, int arguments, List<String> components, String permission) {
-        this.name = name;
+        super(name);
         this.arguments = arguments;
         this.components = components;
         this.permission = "venturechat." + permission;
@@ -26,21 +31,34 @@ public class Alias {
     public static void initialize() {
         aliases = new ArrayList<Alias>();
         ConfigurationSection cs = plugin.getConfig().getConfigurationSection("alias");
+        CommandMap commandMap = getCommandMap(plugin.getServer());
         for (String key : cs.getKeys(false)) {
             String name = key;
             int arguments = cs.getInt(key + ".arguments", 0);
             List<String> components = cs.getStringList(key + ".components");
             String permissions = cs.getString(key + ".permissions", "None");
-            aliases.add(new Alias(name, arguments, components, permissions));
+            Alias alias = new Alias(name, arguments, components, permissions);
+            aliases.add(alias);
+            commandMap.register("venturechat", alias);
         }
     }
 
-    public static List<Alias> getAliases() {
-        return aliases;
+    private static CommandMap getCommandMap(Server server) {
+        try {
+            return server.getCommandMap();
+        } catch (NoSuchMethodError e) {
+            try {
+                Field commandMapField = server.getClass().getDeclaredField("commandMap");
+                commandMapField.setAccessible(true);
+                return (CommandMap) commandMapField.get(server);
+            } catch (ReflectiveOperationException ex) {
+                throw new RuntimeException("Unable to access CommandMap. Use Paper.", ex);
+            }
+        }
     }
 
-    public String getName() {
-        return name;
+    public static List<Alias> all() {
+        return aliases;
     }
 
     public int getArguments() {
@@ -58,4 +76,14 @@ public class Alias {
     public boolean hasPermission() {
         return !permission.equalsIgnoreCase("venturechat.none");
     }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        return false;
+    }
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String label, String[] args) {
+		return Collections.emptyList();
+	}
 }
--- src/main/java/mineverse/Aust1n46/chat/listeners/CommandListener.java
+++ src/main/java/mineverse/Aust1n46/chat/listeners/CommandListener.java
@@ -4,27 +4,18 @@ import java.io.FileNotFoundException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 
-import me.clip.placeholderapi.PlaceholderAPI;
 import mineverse.Aust1n46.chat.MineverseChat;
 import mineverse.Aust1n46.chat.alias.Alias;
 import mineverse.Aust1n46.chat.api.MineverseChatAPI;
 import mineverse.Aust1n46.chat.api.MineverseChatPlayer;
-import mineverse.Aust1n46.chat.channel.ChatChannel;
 import mineverse.Aust1n46.chat.database.Database;
 import mineverse.Aust1n46.chat.utilities.Format;
-import mineverse.Aust1n46.chat.versions.VersionHandler;
 
 public class CommandListener implements Listener {
 	private MineverseChat plugin = MineverseChat.getInstance();
@@ -56,65 +47,68 @@ public class CommandListener implements Listener {
 			}
 		}
 
-		String[] blocked = event.getMessage().split(" ");
-
 		String message = event.getMessage();
 
 		if (Database.isEnabled()) {
 			Database.writeVentureChat(mcp.getUUID().toString(), mcp.getName(), "Local", "Command_Component", event.getMessage().replace("'", "''"), "Command");
 		}
 
-		for (Alias a : Alias.getAliases()) {
-			if (message.toLowerCase().substring(1).split(" ")[0].equals(a.getName().toLowerCase())) {
-				for (String s : a.getComponents()) {
-					if (!mcp.getPlayer().hasPermission(a.getPermission()) && a.hasPermission()) {
-						mcp.getPlayer().sendMessage(ChatColor.RED + "You do not have permission for this alias.");
-						event.setCancelled(true);
-						return;
-					}
-					int num = 1;
-					if (message.length() < a.getName().length() + 2 || a.getArguments() == 0)
-						num = 0;
-					int arg = 0;
-					if (message.substring(a.getName().length() + 1 + num).length() == 0)
-						arg = 1;
-					String[] args = message.substring(a.getName().length() + 1 + num).split(" ");
-					String send = "";
-					if (args.length - arg < a.getArguments()) {
-						String keyword = "arguments.";
-						if (a.getArguments() == 1)
-							keyword = "argument.";
-						mcp.getPlayer().sendMessage(ChatColor.RED + "Invalid arguments for this alias, enter at least " + a.getArguments() + " " + keyword);
-						event.setCancelled(true);
-						return;
-					}
-					for (int b = 0; b < args.length; b++) {
-						send += " " + args[b];
-					}
-					if (send.length() > 0)
-						send = send.substring(1);
-					s = Format.FormatStringAll(s);
-					if (mcp.getPlayer().hasPermission("venturechat.color.legacy")) {
-						send = Format.FormatStringLegacyColor(send);
-					}
-					if (mcp.getPlayer().hasPermission("venturechat.color")) {
-						send = Format.FormatStringColor(send);
-					}
-					if (mcp.getPlayer().hasPermission("venturechat.format")) {
-						send = Format.FormatString(send);
-					}
-					if (s.startsWith("Command:")) {
-						mcp.getPlayer().chat(s.substring(9).replace("$", send));
-						event.setCancelled(true);
-					}
-					if (s.startsWith("Message:")) {
-						mcp.getPlayer().sendMessage(s.substring(9).replace("$", send));
-						event.setCancelled(true);
-					}
-					if (s.startsWith("Broadcast:")) {
-						Format.broadcastToServer(s.substring(11).replace("$", send));
-						event.setCancelled(true);
-					}
+		for (Alias a : Alias.all()) {
+			String cmdName = message.toLowerCase().substring(1).split(" ")[0];
+			String aliasName = a.getName().toLowerCase();
+			String shortLabel = cmdName.contains(":") ? cmdName.substring(cmdName.lastIndexOf(':') + 1) : cmdName;
+			if (!shortLabel.equals(aliasName)) {
+				continue;
+			}
+			int cmdEnd = cmdName.length() + 1;
+			for (String s : a.getComponents()) {
+				if (!mcp.getPlayer().hasPermission(a.getPermission()) && a.hasPermission()) {
+					mcp.getPlayer().sendMessage(ChatColor.RED + "You do not have permission for this alias.");
+					event.setCancelled(true);
+					return;
+				}
+				int num = 1;
+				if (message.length() < cmdEnd + 1 || a.getArguments() == 0)
+					num = 0;
+				int arg = 0;
+				if (message.substring(cmdEnd + num).length() == 0)
+					arg = 1;
+				String[] args = message.substring(cmdEnd + num).split(" ");
+				String send = "";
+				if (args.length - arg < a.getArguments()) {
+					String keyword = "arguments.";
+					if (a.getArguments() == 1)
+						keyword = "argument.";
+					mcp.getPlayer().sendMessage(ChatColor.RED + "Invalid arguments for this alias, enter at least " + a.getArguments() + " " + keyword);
+					event.setCancelled(true);
+					return;
+				}
+				for (int b = 0; b < args.length; b++) {
+					send += " " + args[b];
+				}
+				if (send.length() > 0)
+					send = send.substring(1);
+				s = Format.FormatStringAll(s);
+				if (mcp.getPlayer().hasPermission("venturechat.color.legacy")) {
+					send = Format.FormatStringLegacyColor(send);
+				}
+				if (mcp.getPlayer().hasPermission("venturechat.color")) {
+					send = Format.FormatStringColor(send);
+				}
+				if (mcp.getPlayer().hasPermission("venturechat.format")) {
+					send = Format.FormatString(send);
+				}
+				if (s.startsWith("Command:")) {
+					mcp.getPlayer().chat(s.substring(9).replace("$", send));
+					event.setCancelled(true);
+				}
+				if (s.startsWith("Message:")) {
+					mcp.getPlayer().sendMessage(s.substring(9).replace("$", send));
+					event.setCancelled(true);
+				}
+				if (s.startsWith("Broadcast:")) {
+					Format.broadcastToServer(s.substring(11).replace("$", send));
+					event.setCancelled(true);
 				}
 			}
 		}
